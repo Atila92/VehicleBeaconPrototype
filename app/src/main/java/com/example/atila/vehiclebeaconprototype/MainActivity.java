1ab90dc721f90d786b2406da84dcd29d48ecddf1
Atila92 <atila.sarvari@gmail.com>
2017-10-01 11:38:34 +0200

    Setup

--- app/src/main/java/com/example/atila/vehiclebeaconprototype/MainActivity.java
+++ app/src/main/java/com/example/atila/vehiclebeaconprototype/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.atila.vehiclebeaconprototype;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.estimote.coresdk.common.config.EstimoteSDK;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Allows the SDK to communicate to the cloud
+        EstimoteSDK.initialize(getApplicationContext(), "vehiclebeaconprototype-j17", "907e4bf380bf666da2634cf7c8fbec02");
+
     }
 }
